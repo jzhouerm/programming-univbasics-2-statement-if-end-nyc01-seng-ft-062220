f5cefd5e68853ed5287d8d2bf5a903c4ff88fd96
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 16:47:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 chance_of_rain = 0.2
 puts "Let's go outside!"
-if chance_of_rain =< 0.25
+if chance_of_rain <= 0.25
 elsif (chance_of_rain > 0.25 && chance_of_rain <0.75)
   puts "Pack an umbrella!"
 else
