6d36e800e4d52e901ee85e8b1d75e5b1e6240ceb
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 16:47:16 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,8 @@
 chance_of_rain = 0.2
 puts "Let's go outside!"
 if chance_of_rain =< 0.25
-  
+elsif (chance_of_rain > 0.25 && chance_of_rain <0.75)
+  puts "Pack an umbrella!"
+else
+  puts"Stay home and read Hegel."
+end
