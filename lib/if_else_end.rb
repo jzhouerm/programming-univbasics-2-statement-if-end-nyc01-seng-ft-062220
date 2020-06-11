d1357c7e516b550858b3db8c48c391c25c199af3
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 16:41:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,4 @@
-chance_of_rain = 0.6
+chance_of_rain = 0.2
 puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+if chance_of_rain =< 0.25
+  
