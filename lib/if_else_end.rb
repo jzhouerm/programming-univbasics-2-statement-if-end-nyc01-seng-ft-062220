459d8ca84095f66d4cdf9532e592e2799fcca5c5
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 17:24:25 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,13 @@
 current_time = Time.now 
 current_time = current_time.to_i 
-if current_time % 2 == 0 
-  puts "Even!"
-else 
-  puts "Odd!"
-end 
+#if current_time % 2 == 0 
+ # puts "Even!"
+#else 
+ # puts "Odd!"
+#end 
 
+if current_time
+  puts "Even!"
+else
+  puts "Odd!"
+end
